09761ae1904b246387beb22bca5ad155a8964495
aftlah <altaf.fattah05@gmail.com>
2024-08-03 12:26:21 +0700

    navbar layouts

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,135 +1,274 @@
-'use client'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Bookmark, Heart, KanbanIcon, MessageSquare, MoreHorizontal, Pencil, Repeat2, Share2 } from 'lucide-react'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Bookmark,
+  Heart,
+  KanbanIcon,
+  MessageSquare,
+  MoreHorizontal,
+  Pencil,
+  Repeat2,
+  Settings,
+  Share2,
+} from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-
+import {
+  Form,
+  FormControl,
+  // FormDescription,
+  FormField,
+  FormItem,
+  // FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
 
 const data = [
   {
     id: 1,
-    username: 'pulu pulu',
-    avatar: '',
-    contents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!',
+    username: "pulu pulu",
+    avatar: "",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!",
   },
   {
     id: 2,
-    username: 'aftalh',
-    avatar: '',
-    contents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!',
+    username: "aftalh",
+    avatar: "",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!",
   },
   {
     id: 3,
-    username: 'dlifalde',
-    avatar: '',
-    contents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!',
+    username: "dlifalde",
+    avatar: "",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!",
   },
   {
     id: 4,
-    username: 'nawreen',
-    avatar: '',
-    contents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!',
+    username: "nawreen",
+    avatar: "",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!",
   },
   {
     id: 5,
-    username: 'narenath',
-    avatar: '',
-    contents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!',
+    username: "narenath",
+    avatar: "",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!",
   },
   {
     id: 6,
-    username: 'coki guslaw',
-    avatar: '',
-    contents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!',
+    username: "coki guslaw",
+    avatar: "",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus. Mollitia laboriosam eos, culpa sed porro pariatur!",
   },
   {
     id: 7,
-    username: 'ronaldonyo',
-    avatar: '',
-    contents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus.'
-  }
-]
-
+    username: "ronaldonyo",
+    avatar: "",
+    contents:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem repudiandae corrupti magnam quos, perspiciatis at quia, ducimus accusantium possimus corporis aliquid quis minus.",
+  },
+];
 
 const HomePage = () => {
-  const [scroll, setScroll] = useState(false)
+  const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 100) {
-        setScroll(true)
+        setScroll(true);
       } else {
-        setScroll(false)
+        setScroll(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [scroll])
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const FormSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
 
   return (
-    <main className='mt-32 realtive sm:mt-0 '>
-      <Link href={''} className={`fixed py-2 right-5  size-14 bottom-24 z-50 rounded-full hover:opacity-90 bg-blue-500 ${scroll ? 'opacity-50' : "opacity-100"}`}>
-        <div className='w-fit  h-full justify-center m-auto flex items-center'>
-          <Pencil className="size-6 " />
-        </div>
-      </Link>
-
-      {data?.map((data, i) => (
-        <div key={data.id} className=' px-5 py-5 grid border grid-cols-8 sm:grid-cols-12 content-center sm:gap-x-5 md:gap-x-0 '>
-          <Avatar className='col-span-1'>
-            <Link href={''}>
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" className="size-10" />
-              <AvatarFallback>Profile</AvatarFallback>
-            </Link>
+    <>
+      <header className="hidden sm:flex w-full justify-between  border pr-5 ">
+        <Link
+          href={""}
+          className={
+            "text-sm  w-[100%]  text-center font-bold cursor-pointer   "
+          }
+        >
+          <p className="border-blue-500 border-b-4 py-4 w-28 mx-auto">
+            For you
+          </p>
+        </Link>
+        <Link
+          href={"/following"}
+          className={
+            "text-sm font-normal text-center  w-[100%] cursor-pointer  text-gray-400  "
+          }
+        >
+          <p className="border-blue-500  border-b-4 w-28 py-4 mx-auto">
+            Following
+          </p>
+        </Link>
+        <Link href={""} className=" flex  items-center   h-auto">
+          <div className="hover:bg-zinc-800 p-1.5 rounded-full">
+            <Settings />
+          </div>
+        </Link>
+      </header>
+      <div className="hidden sm:block p-5 border ">
+        <div className="flex items-start gap-x-2">
+          <Avatar>
+            <AvatarImage
+              src="https://github.com/shadcn.png"
+              alt="@shadcn"
+              className="size-10"
+            />
+            <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <div className='col-span-7 sm:col-span-10 '>
-            <div className='flex justify-between mb-1'>
-              <Link href={''} className='font-bold hover:underline'>
-                {data.username}
-              </Link>
-              <Link href={''}>
-                <MoreHorizontal />
-              </Link>
-            </div>
-            <Link href={''}>
-              <p className='text-xs md:text-sm'>
-                {data.contents}
-              </p>
-            </Link>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-[87%] space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    {/* <FormLabel>Username</FormLabel> */}
+                    <FormControl className="bg-red-800">
+                      <Input
+                        type="text"
+                        className="text-xl h-auto font-light border-0 bg-transparent"
+                        placeholder="What is happening?!"
+                        {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>
+                                                This is your public display name.
+                                            </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="ml-auto rounded-3xl px-6 bg-blue-500 text-white font-semibold hover:bg-blue-500"
+              >
+                Post
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+      <main className="mt-32 realtive sm:mt-0 ">
+        <Link
+          href={""}
+          className={`fixed py-2 right-5 lg:hidden  size-14 bottom-24 z-50 rounded-full hover:opacity-90 bg-blue-500 ${
+            scroll ? "opacity-50" : "opacity-100"
+          }`}
+        >
+          <div className="w-fit  h-full justify-center m-auto flex items-center">
+            <Pencil className="size-6 " />
+          </div>
+        </Link>
 
-            <div className='flex text-sm gap-x-3 text-gray-500 pt-4 items-center'>
-              <Link href={''} className='flex gap-1 items-center px-1'>
-                <MessageSquare className="size-4" />
-                <span>2.3k</span>
+        {data?.map((data, i) => (
+          <div
+            key={data.id}
+            className=" px-5 py-5 grid border grid-cols-8 sm:grid-cols-12 content-center sm:gap-x-5 md:gap-x-0 "
+          >
+            <Avatar className="col-span-1">
+              <Link href={""}>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                  className="size-10"
+                />
+                <AvatarFallback>Profile</AvatarFallback>
               </Link>
-              <Link href={''} className='flex gap-1 items-center px-1'>
-                <Repeat2 className="size-4" />
-                <span>480</span>
+            </Avatar>
+            <div className="col-span-7 sm:col-span-10 ">
+              <div className="flex justify-between mb-1">
+                <Link href={""} className="font-bold hover:underline">
+                  {data.username}
+                </Link>
+                <Link href={""}>
+                  <MoreHorizontal />
+                </Link>
+              </div>
+              <Link href={""}>
+                <p className="text-xs md:text-sm">{data.contents}</p>
               </Link>
-              <Link href={''} className='flex gap-1 items-center px-1'>
-                <Heart className="size-4" />
-                <span>1.5K</span>
-              </Link>
-              <Link href={''} className='flex gap-1 items-center px-1'>
-                <KanbanIcon className="size-4 rotate-180" />
-                <span>80K</span>
-              </Link>
-              <Link href={''} className='flex items-center'>
-                <Bookmark className="size-4" />
-              </Link>
-              <Link href={''} className='flex items-center'>
-                <Share2 className="size-4" />
-              </Link>
+
+              <div className="flex text-sm gap-x-3 text-gray-500 pt-4 items-center">
+                <Link href={""} className="flex gap-1 items-center px-1">
+                  <MessageSquare className="size-4" />
+                  <span>2.3k</span>
+                </Link>
+                <Link href={""} className="flex gap-1 items-center px-1">
+                  <Repeat2 className="size-4" />
+                  <span>480</span>
+                </Link>
+                <Link href={""} className="flex gap-1 items-center px-1">
+                  <Heart className="size-4" />
+                  <span>1.5K</span>
+                </Link>
+                <Link href={""} className="flex gap-1 items-center px-1">
+                  <KanbanIcon className="size-4 rotate-180" />
+                  <span>80K</span>
+                </Link>
+                <Link href={""} className="flex items-center">
+                  <Bookmark className="size-4" />
+                </Link>
+                <Link href={""} className="flex items-center">
+                  <Share2 className="size-4" />
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </main>
-  )
-}
+        ))}
+      </main>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/components/navbarLayouts/index.tsx
+++ src/components/navbarLayouts/index.tsx
@@ -1,31 +1,21 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import {
     Avatar,
     AvatarFallback,
     AvatarImage,
 } from "@/components/ui/avatar"
-import {
-    Form,
-    FormControl,
-    // FormDescription,
-    FormField,
-    FormItem,
-    // FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
+
 import LogoX from "@/components/Logo/LogoX";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Navbar from "@/components/atoms/Navbar";
 import { Bell, Bookmark, CircleEllipsis, Home, Mail, NotepadText, Search, Settings, User, UserRound, UsersRound, MoreHorizontal } from 'lucide-react';
 import { Input } from '@/components/ui/input';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+
 import { z } from 'zod';
-import { toast } from '@/components/ui/use-toast';
 import { SizeIcon } from '@radix-ui/react-icons';
 
 
@@ -33,37 +23,37 @@ const link = [
     {
         id: 1,
         logo: <Home className="size-7" />,
-        href: '',
+        href: '/home',
         title: 'Home'
     },
     {
         id: 2,
         logo: <Search className="size-7" />,
-        href: '',
+        href: '/search',
         title: 'Search'
     },
     {
         id: 3,
         logo: <Bell className="size-7" />,
-        href: '',
+        href: 'notifications',
         title: 'Notification'
     },
     {
         id: 4,
         logo: <Mail className="size-7" />,
-        href: '',
+        href: '/messages',
         title: 'Message'
     },
     {
         id: 5,
         logo: <NotepadText className="size-7" />,
-        href: '',
+        href: '/progile_name/lists',
         title: 'Note'
     },
     {
         id: 6,
         logo: <Bookmark className="size-7" />,
-        href: '',
+        href: '/bookmark',
         title: 'Bookmark'
     },
     {
@@ -75,13 +65,13 @@ const link = [
     {
         id: 8,
         logo: <UserRound className="size-7" />,
-        href: '',
+        href: '/profile',
         title: 'Profile'
     },
     {
         id: 9,
         logo: <CircleEllipsis className="size-7" />,
-        href: '',
+        href: 'seting',
         title: 'More'
     }
 ]
@@ -116,7 +106,7 @@ const data = [
 ]
 
 
-export default function HomeLayout({ children }: { children: ReactNode }) {
+export default function IndexLayout({ children }: { children: ReactNode }) {
 
     const [scrolling, setScrolling] = useState(false)
     const controls = useAnimation()
@@ -141,33 +131,6 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
             window.removeEventListener('scroll', handleScroll)
         }
     }, [scrolling, controls])
-
-    const FormSchema = z.object({
-        username: z.string().min(2, {
-            message: "Username must be at least 2 characters.",
-        }),
-    })
-
-
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            username: "",
-        },
-    })
-
-    function onSubmit(data: z.infer<typeof FormSchema>) {
-        toast({
-            title: "You submitted the following values:",
-            description: (
-                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-                </pre>
-            ),
-        })
-    }
-
-
 
     return (
         <div className='sm:grid grid-cols-12 lg:grid-cols-6 w-full content-center overflow-x-hidden'>
@@ -202,8 +165,8 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
                     </div>
 
                     <div className="flex items-center justify-around">
-                        <Link href={''} className={`text-sm font-bold cursor-pointer border-b-4 py-3 border-blue-500 `}>For you</Link>
-                        <Link href={''} className={`text-sm font-normal cursor-pointer py-3 text-gray-400 border-b-4 border-blue-500`}>Following</Link>
+                        <Link href={''} className={"text-sm font-bold cursor-pointer border-b-4 py-3 border-blue-500 "}>For you</Link>
+                        <Link href={''} className={"text-sm font-normal cursor-pointer py-3 text-gray-400 border-b-4 border-blue-500"}>Following</Link>
                     </div>
                 </header>
             </motion.div>
@@ -229,53 +192,10 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
                     </Avatar>
                 </Link>
             </div>
-
             <div className=' col-span-10 col-start-3 lg:col-span-3 lg:col-start-2 max-w-2xl  '>
-                <header className='hidden sm:flex w-full justify-between  border pr-5 '>
-                    <Link href={''} className={`text-sm  w-[100%]  text-center font-bold cursor-pointer   `}>
-                        <p className='border-blue-500 border-b-4 py-4 w-28 mx-auto'>For you</p>
-                    </Link>
-                    <Link href={'/following'} className={`text-sm font-normal text-center  w-[100%] cursor-pointer  text-gray-400  `}>
-                        <p className='border-blue-500  border-b-4 w-28 py-4 mx-auto'>Following</p>
-                    </Link>
-                    <Link href={''} className=' flex  items-center   h-auto'>
-                        <div className='hover:bg-zinc-800 p-1.5 rounded-full'>
-                            <Settings />
-                        </div>
-                    </Link>
-                </header>
-                <div className='hidden sm:block p-5 border '>
-                    <div className='flex items-start gap-x-2'>
-                        <Avatar>
-                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" className="size-10" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                        <Form {...form}>
-                            <form onSubmit={form.handleSubmit(onSubmit)} className="w-[87%] space-y-6">
-                                <FormField
-                                    control={form.control}
-                                    name="username"
-                                    render={({ field }) => (
-                                        <FormItem >
-                                            {/* <FormLabel>Username</FormLabel> */}
-                                            <FormControl className='bg-red-800'>
-                                                <Input type='text' className='text-xl h-auto font-light border-0 bg-transparent' placeholder="What is happening?!" {...field} />
-                                            </FormControl>
-                                            {/* <FormDescription>
-                                                This is your public display name.
-                                            </FormDescription> */}
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                                <Button type="submit" className="ml-auto rounded-3xl px-6 bg-blue-500 text-white font-semibold hover:bg-blue-500">Post</Button>
-                            </form>
-                        </Form>
-                    </div>
-                </div>
+                
                 {children}
             </div>
-
             <div className='hidden lg:block col-span-2  h-full mx-auto'>
                 <div className='border flex flex-row items-center rounded-full my-3 px-2'>
                     <div className='px-2'>
@@ -289,7 +209,6 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
                         <p className='text-sm'>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
                         <Button className='bg-blue-500 text-white rounded-3xl w-fit px-5 py-1 hover:bg-blue-400'>Subscribe</Button>
                     </div>
-
                     <div className='bg-transparent p-4 rounded-2xl flex flex-col gap-y-5 border'>
                         <h1 className='font-bold text-lg'>Trends for you</h1>
                         {data.map((data, _) => (
@@ -307,7 +226,6 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
                         <Link href={''} className='text-blue-500 text-sm'>
                             Show more
                         </Link>
-
                     </div>
                 </div>
             </div>
